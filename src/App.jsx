dcf85d72b343c89b5b468c684e5b4d7ebd89bc67
Even <even.berhane2022@gmail.com>
2024-06-23 11:41:09 +0300

    viewport scale

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Button from './components/Button';
+import { useState } from 'react'
+import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className='parent-div'>
-      <nav className='navbar'>
-        <div className='logo'>
-          <h1>Logo</h1>
-        </div>
-      </nav>
-      <div className='input-div'>
-        <h1>Create Short URLs</h1>
-        <input className='link-input' type='text' placeholder='Paste Link Here'></input>
-        <Button />
-
-        <div className='description'>
-          <h3>Link House is a free tool to shorten URLs and generate short links.
-            URL shortener allows you to create a shortened link making it easy to share.</h3>
-        </div>
+  <div className='parent-div'>
+    <div className='input-div'>
+        
+      <h1>Create Short URLs</h1>
+      <input className='link-input' type='text' placeholder='Paste Link Here'></input>
+      <div className='description'> 
+      <h3>Link House is a free tool to shorten URLs and generate short links
+      URL shortener allows to create a shortened link making it easy to share</h3>
+      </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
