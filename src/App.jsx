02949f4f65d06d2cf030e5605a0176bf03c87485
Even <even.berhane2022@gmail.com>
2024-06-23 11:29:12 +0300

    imported button component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-
+import React, { useState } from 'react';
+import './App.css';
+import Button from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  
-    <div className='input-div'>
-        
-      <h1>Create Short URLs</h1>
-      <input className='link-input' type='text'></input>
-      <div className='description'> 
+    <div className='parent-div'>
+      <nav className='navbar'>
+        <div className='logo'>
+          <h1>Logo</h1>
+        </div>
+      </nav>
+      <div className='input-div'>
+        <h1>Create Short URLs</h1>
+        <input className='link-input' type='text' placeholder='Paste Link Here'></input>
+        <Button />
 
+        <div className='description'>
+          <h3>Link House is a free tool to shorten URLs and generate short links.
+            URL shortener allows you to create a shortened link making it easy to share.</h3>
+        </div>
       </div>
-      <h3>Link House is a free tool to shorten URLs and generate short links
-      URL shortener allows to create a shortened link making it easy to share</h3>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
