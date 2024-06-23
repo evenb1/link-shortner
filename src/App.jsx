26c0030a43f0835d04969391761020aacbdb34d1
Even <even.berhane2022@gmail.com>
2024-06-23 23:32:55 +0300

    background animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +9,11 @@ function App() {
     <div className='input-div'>
         
       <h1>Create Short URLs</h1>
+      <div className='input-button'>
       <input className='link-input' type='text' placeholder='Paste Link Here'></input>
-      <div className='description'> 
+      <Button />
+      </div>
+        <div className='description'> 
       <h3>Link House is a free tool to shorten URLs and generate short links
       URL shortener allows to create a shortened link making it easy to share</h3>
       </div>
