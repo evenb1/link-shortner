49a400cee5b6940827369e0a7d7e2eb19a23acc7
Even <even.berhane2022@gmail.com>
2024-06-23 11:12:29 +0300

    h3 description and css changes to style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +11,11 @@ function App() {
         
       <h1>Create Short URLs</h1>
       <input className='link-input' type='text'></input>
+      <div className='description'> 
+
+      </div>
+      <h3>Link House is a free tool to shorten URLs and generate short links
+      URL shortener allows to create a shortened link making it easy to share</h3>
     </div>
   )
 }
