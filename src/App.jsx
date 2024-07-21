82869ef766fdf40c365e8b90301b8168beac4c68
Even <even.berhane2022@gmail.com>
2024-07-21 19:30:32 +0300

    button onclick event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import Button from './components/Button'
 
-const App = () => {
-  const [url, setUrl] = useState('');
-  const [shortenedUrl, setShortenedUrl] = useState('');
+function App() {
+  const [url, setUrl] = useState('')
+  const [shortenedUrl, setShortenedUrl] = useState('')
 
   const handleShorten = async () => {
     try {
@@ -20,33 +20,42 @@ const App = () => {
             'Content-Type': 'application/json',
           },
         }
-      );
-      setShortenedUrl(response.data.link);
+      )
+      setShortenedUrl(response.data.link)
     } catch (error) {
-      console.error('Error shortening URL:', error);
+      console.error('Error shortening URL:', error)
     }
-  };
+  }
 
   return (
-    <div className="App">
-      <h1>URL Shortener</h1>
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Enter your URL here"
-      />
-      <button onClick={handleShorten}>Shorten</button>
+    <div className='input-div'>
+      <h1>Create Short URLs</h1>
+      <div className='input-button'>
+        <input
+          className='link-input'
+          type='text'
+          placeholder='Paste Link Here'
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <Button onClick={handleShorten} />
+      </div>
       {shortenedUrl && (
         <div>
           <p>Shortened URL:</p>
-          <a href={shortenedUrl} target="_blank" rel="noopener noreferrer">
+          <a href={shortenedUrl} target='_blank' rel='noopener noreferrer'>
             {shortenedUrl}
           </a>
         </div>
       )}
+      <div className='description'>
+        <h3>
+          Link House is a free tool to shorten URLs and generate short links.
+          URL shortener allows you to create a shortened link, making it easy to share.
+        </h3>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App
