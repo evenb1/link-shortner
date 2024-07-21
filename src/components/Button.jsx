6b9897a4dfe65dcc1f8fda76fcc07d28addd303e
Even <even.berhane2022@gmail.com>
2024-07-21 19:49:46 +0300

    button function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 
 const Button = ({ onClick }) =>  {
   return (
-    <div><button onClick={onClick}><span>Shorten URL</span></button>
+    <div><button onClick={onClick}>Shorten URL</button>
 </div>
   )
 }
